fa41b5c1fe3e75c1b2f9374456d59b10b470b50e
Juancho10 <juanitolizarazu18@gmail.com>
2023-04-23 20:25:09 -0500

    mi primer componente sin estilos y mejorado

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+export function App() {
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/github/37t?fallback=https://avatars.githubusercontent.com/u/66378906?v=4" alt="avatar" />
+                <div>
+                    <strong> Tom & Jerry</strong>
+                    <span>@Tomy</span>
+                </div>    
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    );
+}
